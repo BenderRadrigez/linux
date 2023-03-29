005794044aefa8be4226e1bb5b97da3d72f2c74a
Andrei Petrusevich <111006214+BenderRadrigez@users.noreply.github.com>
2023-03-29 09:46:08 +0200

    Add files via upload
    Выполняйте в песочнице
    
    Условие задания:
    1 Создайте скрипт sleeper.sh, который будет 10 раз с интервалом в 5 секунд писать дату в формате HH:MM:SS и количество процессов одним числом.
    2 Уменьшите или уберите временной интервал (который нам дает sleep), используя vi или nano, закомментировав строку или поменяв значение sleep
    3 С помощью скрипта запишите в файл информацию о процессоре. 
    4 С помощью скрипта запишите в файл информацию об операционной системе, но отфильтруйте информацию так, чтобы осталось только имя (NAME=Alpine Linux).
    5 Выполните прошлое действие, но так, чтобы слово NAME= не осталось, а было только имя в чистом виде (Alpine)
    6 С помощью скрипта создайте 50 файлов с расширением txt и именами от 50.txt до 100.txt
    Вот что вам понадобится для реализации всего этого: 
    Если удобно, то вот однострочник, создающий простейший скрипт, который надо будет редактировать: 
    echo -e #!/bin/bash\n date\n echo “it works!“ > /tmp/script.sh

---
+++ HomeWork2403/sleeper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+for i in {1..10} 
+do
+echo "сейчас $(date +'%H:%M:%S') времени, и $(ps aux | wc -l) процессов"
+sleep .5
+done
+
+lscpu > cpu_info.txt
+cat /etc/os-release | head -1 >> os_info.txt 
+cat /etc/os-release | head -1 | sed 's/NAME="//' | cut -d' ' -f1 >> os_info.txt
+ 
+for i in {50..100} 
+do
+touch "${i}.txt"
+done
